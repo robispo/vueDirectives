2fb024f29b74e0cef0d3cad280255404b0bd883b
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-21 20:52:11 -0500

    Global directive without args

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,11 @@
         <p v-html="'<strong>html directive</strong>'"></p>
       </div>
     </div>
-    <hr>
+    <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
+        <p v-highlight>highlight directive</p>
       </div>
     </div>
   </div>
