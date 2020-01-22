c2eca387968ff8c76974e3b1d6d111a68e243065
Rabel Obispo <rabelobispo@hotmail.com>
2020-01-21 20:39:20 -0500

    Init commit and build-in directive

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Build-in Directives</h1>
+
+        <p v-text="'text directive'"></p>
+        <p v-html="'<strong>html directive</strong>'"></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
